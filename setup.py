e7ad5cd0d793f4202adcab0156eafb03f47722de
CasAndreu <acasas2@uw.edu>
2016-07-26 15:56:57 -0700

    adding basic files

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='robust_lda',
+setup(name='rlda',
       version='0.1',
       description='A module to use robust lda topics for the study of text',
-      url='http://github.com/CasAndreu/robust_lda',
+      url='http://github.com/CasAndreu/rlda',
       author='Andreu Casas',
       author_email='acasas2@uw.edu',
       license='MIT',
-      packages=['robust_lda'],
+      packages=['rlda'],
       zip_safe=False)
