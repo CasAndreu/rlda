5f52780a8f23afc52577a39a2d44bcf814643e0e
CasAndreu <acasas2@uw.edu>
2016-07-26 15:06:38 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='robust_lda',
+      version='0.1',
+      description='A module to use robust lda topics for the study of text',
+      url='http://github.com/CasAndreu/robust_lda',
+      author='Andreu Casas',
+      author_email='acasas2@uw.edu',
+      license='MIT',
+      packages=['robust_lda'],
+      zip_safe=False)
